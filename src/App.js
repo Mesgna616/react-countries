9c228f72b50146a8c743d6eb24646ff712e336a6
mesgna woldeb <mesgna.mehari8@gmail.com>
2023-01-01 21:23:40 +0000

    until level 300 completed

--- src/App.js
+++ src/App.js
@@ -1,24 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import AllCountries from "./AllCountries.json";
+import CardHolder from "./CardHolder.js";
+import "./App.css";
 
 function App() {
+  const regions = [...new Set(AllCountries.map((country) => country.region))];
+  const [countries, setCountries] = useState(AllCountries);
+  const [regionsFilter, setRegionsFilter] = useState("All Regions");
+  const [countriesFilter, setCountriesFilter] = useState("");
+
+  useEffect(() => {
+    console.log(AllCountries);
+    setCountries(
+      AllCountries.filter(
+        (country) =>
+          (regionsFilter === "All Regions" ||
+            country.region
+              .toLocaleLowerCase()
+              .includes(regionsFilter.toLocaleLowerCase())) &&
+          (countriesFilter === "" ||
+            country.name
+              .toLocaleLowerCase()
+              .includes(countriesFilter.toLocaleLowerCase()))
+      )
+    );
+  }, [regionsFilter, countriesFilter]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn 
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="form-row m-5">
+        <input id="input"
+         className="form-control col-md-9 m-2"
+         type= "search"
+         placeholder=" search for countries"
+         onChange={(e)=>setCountriesFilter(e.target.value)}
+         />
+         <select
+          id = "select"
+          onChange={(e)=>setRegionsFilter(e.target.value)}
+          className="form-control col-md-2 m-2"
+          >
+         <option> All Regions</option>
+         {regions.map((region)=> (
+          <option>{region}</option>))}
+         </select>
+      </div>
+      <div className="card-deck p-1">
+        {countries.map((country, index) => (
+          <CardHolder data={country} index={index} />
+        ))}
+      </div>
+    </>
   );
 }
 
---
+++ src/CardHolder.js
@@ -0,0 +1,27 @@
+import React, { useState } from "react";
+const CardHolder = (props) => {
+  const [classDiv, setClassDiv] = useState(true);
+  return (
+    <div
+      key={props.index}
+      onClick={() => setClassDiv(!classDiv)}
+      className={classDiv ? "card card-body" : "country-detail"}
+    >
+      <img alt={props.data.name} src={props.data.flag} className="thumbnail" />
+      <div className="content">
+        <h6> {props.data.name}</h6>
+        <strong> Population : </strong>
+        {props.data.population.toLocaleString()}
+        <br />
+        <strong> Region :</strong>
+        {props.data.region}
+        <br />
+        <strong> Capital :</strong>
+        {props.data.capital}
+       
+        <br />
+      </div>
+    </div>
+  );
+};
+export default CardHolder
